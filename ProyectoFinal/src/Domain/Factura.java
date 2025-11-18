b78e106a58cdab16e799ba482d45c28a8b659365
AlexaMiranda30 <milalex3003@gmail.com>
2025-11-17 21:11:02 -0600

    Factura

--- ProyectoFinal/src/Domain/Factura.java
+++ ProyectoFinal/src/Domain/Factura.java
@@ -1,7 +1,52 @@
 
 package Domain;
 
+import java.text.DecimalFormat;
+
 public class Factura {
-    
- 
+
+    private final double PORCENTAJE_IVA = 0.13;
+
+    private final String numeroFactura;
+    private final String numeroTiquete;
+    private final String nombreCliente;
+    private final String cedulaCliente;
+    private final double montoBase;
+    private final double montoIVA;
+    private final double montoTotal;
+    private final String claseAsiento;
+
+    public Factura(Tiquete tiquete, String numeroFactura) {
+
+        this.nombreCliente = tiquete.getPasajero().getNombre();
+        this.cedulaCliente = tiquete.getPasajero().getCedula();
+        this.numeroTiquete = tiquete.getCodigoTiquete();
+        this.montoBase = tiquete.getPrecio();
+        this.claseAsiento = tiquete.getClase();
+        this.numeroFactura = numeroFactura;
+
+        this.montoIVA = montoBase * PORCENTAJE_IVA;
+        this.montoTotal = montoBase + montoIVA;
+    }
+
+    public void mostrarFactura() {
+        DecimalFormat df = new DecimalFormat("#,##0.00");
+
+        System.out.println("\n=============================================");
+        System.out.println("          FACTURA DEL PASAJERO");
+        System.out.println("Numero Factura: " + numeroFactura);
+        System.out.println("Tiquete: " + numeroTiquete);
+        System.out.println("---------------------------------------------");
+
+        System.out.println("CLIENTE: " + nombreCliente);
+        System.out.println("Identificación: " + cedulaCliente);
+        System.out.println("Clase de Asiento: " + claseAsiento);
+        System.out.println("---------------------------------------------");
+
+        System.out.println("Costo Base (Sin Imp.): " + df.format(montoBase));
+        System.out.println("IVA (" + (int) (PORCENTAJE_IVA * 100) + "%): " + df.format(montoIVA));
+        System.out.println("---------------------------------------------");
+        System.out.println("MONTO TOTAL PAGADO:    " + df.format(montoTotal));
+        System.out.println("=============================================\n");
+    }
 }
